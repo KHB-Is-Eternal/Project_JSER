952f4d2e445bdcee60db5206b26bf4f1fc75a0cc
khee8469 <bcw1992@naver.com>
2026-04-07 19:59:12 +0900

    Refactor: GE_AddTag Blueprint to C++ class

---
+++ Source/ProjectER/Monster/GAS/GE/GE_AddTag.cpp
@@ -0,0 +1,7 @@
+﻿#include "Monster/GAS/GE/GE_AddTag.h"
+
+UGE_AddTag::UGE_AddTag()
+{
+	DurationPolicy = EGameplayEffectDurationType::Infinite;
+	
+}
---
+++ Source/ProjectER/Monster/GAS/GE/GE_AddTag.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GE_AddTag.generated.h"
+
+
+UCLASS()
+class PROJECTER_API UGE_AddTag : public UGameplayEffect
+{
+	GENERATED_BODY()
+	
+	UGE_AddTag();
+};
