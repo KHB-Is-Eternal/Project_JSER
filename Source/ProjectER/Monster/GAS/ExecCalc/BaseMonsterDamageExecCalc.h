28c8ef982e22f16beb6688b827cd521a80b8ed46
khee8469 <bcw1992@naver.com>
2026-04-10 16:58:22 +0900

    test용 GameplayEffectExecutionCalculation 생성

---
+++ Source/ProjectER/Monster/GAS/ExecCalc/BaseMonsterDamageExecCalc.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "BaseMonsterDamageExecCalc.generated.h"
+
+UCLASS()
+class PROJECTER_API UBaseMonsterDamageExecCalc : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+	
+	UBaseMonsterDamageExecCalc();
+
+	virtual void Execute_Implementation(
+		const FGameplayEffectCustomExecutionParameters& ExecutionParams, 
+		FGameplayEffectCustomExecutionOutput& OutExecutionOutput
+	) const override;
+};
