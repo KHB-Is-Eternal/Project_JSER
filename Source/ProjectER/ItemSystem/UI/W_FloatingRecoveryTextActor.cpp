f3c2048fb582a3f2eb9294d6542db2c213bc8324
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2026-04-24 16:59:25 +0900

    Fix W_FloatingRecoveryTextActor
    회복량 표시 텍스트를 뷰포트 최상단으로 이동

--- Source/ProjectER/ItemSystem/UI/W_FloatingRecoveryTextActor.cpp
+++ Source/ProjectER/ItemSystem/UI/W_FloatingRecoveryTextActor.cpp
@@ -1,8 +1,10 @@
-#include "ItemSystem/UI/W_FloatingRecoveryTextActor.h"
+﻿#include "ItemSystem/UI/W_FloatingRecoveryTextActor.h"
 #include "ItemSystem/UI/W_FloatingRecoveryText.h"
 
 #include "Components/SceneComponent.h"
 #include "Components/WidgetComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "Kismet/GameplayStatics.h"
 
 AW_FloatingRecoveryTextActor::AW_FloatingRecoveryTextActor()
 {
@@ -12,45 +14,21 @@ AW_FloatingRecoveryTextActor::AW_FloatingRecoveryTextActor()
 	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
 	RootComponent = SceneRoot;
 
+	// 이제 이 컴포넌트는 "BP에서 WidgetClass를 지정하는 용도"로만 사용
 	WidgetComp = CreateDefaultSubobject<UWidgetComponent>(TEXT("WidgetComp"));
 	WidgetComp->SetupAttachment(RootComponent);
 	WidgetComp->SetWidgetSpace(EWidgetSpace::Screen);
 	WidgetComp->SetDrawAtDesiredSize(true);
 	WidgetComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	WidgetComp->SetVisibility(false);
 }
 
 void AW_FloatingRecoveryTextActor::BeginPlay()
 {
 	Super::BeginPlay();
-}
 
-void AW_FloatingRecoveryTextActor::Tick(float DeltaSeconds)
-{
-	Super::Tick(DeltaSeconds);
-
-	ElapsedTime += DeltaSeconds;
-
-	// ���� �̵�
-	FVector NewLocation = GetActorLocation();
-	NewLocation.Z += FloatUpSpeed * DeltaSeconds;
-	SetActorLocation(NewLocation);
-
-	// ���� ����
-	const float Alpha = 1.0f - (ElapsedTime / FMath::Max(LifeSeconds, KINDA_SMALL_NUMBER));
-
-	if (WidgetComp)
-	{
-		if (UW_FloatingRecoveryText* Widget = Cast<UW_FloatingRecoveryText>(WidgetComp->GetUserWidgetObject()))
-		{
-			Widget->SetRecoveryAlpha(Alpha);
-		}
-	}
-
-	// ���� ����
-	if (ElapsedTime >= LifeSeconds)
-	{
-		Destroy();
-	}
+	// 로컬 플레이어 컨트롤러 캐싱
+	CachedLocalPC = UGameplayStatics::GetPlayerController(this, 0);
 }
 
 void AW_FloatingRecoveryTextActor::InitRecoveryText(int32 Amount, bool bIsMana)
@@ -60,14 +38,102 @@ void AW_FloatingRecoveryTextActor::InitRecoveryText(int32 Amount, bool bIsMana)
 		return;
 	}
 
-	WidgetComp->InitWidget();
+	if (!CachedLocalPC)
+	{
+		CachedLocalPC = UGameplayStatics::GetPlayerController(this, 0);
+	}
 
-	UW_FloatingRecoveryText* Widget = Cast<UW_FloatingRecoveryText>(WidgetComp->GetUserWidgetObject());
-	if (!Widget)
+	if (!CachedLocalPC || !CachedLocalPC->IsLocalController())
 	{
 		return;
 	}
 
-	Widget->SetRecoveryText(Amount, bIsMana);
-	Widget->SetRecoveryAlpha(1.0f);
+	// BP_FloatingRecoveryTextActor의 WidgetComp에 지정해둔 WidgetClass를 그대로 재사용
+	TSubclassOf<UUserWidget> WidgetClass = WidgetComp->GetWidgetClass();
+	if (!WidgetClass)
+	{
+		return;
+	}
+
+	// 이미 있으면 재사용, 없으면 생성
+	if (!FloatingWidget)
+	{
+		FloatingWidget = CreateWidget<UW_FloatingRecoveryText>(CachedLocalPC, WidgetClass);
+		if (!FloatingWidget)
+		{
+			return;
+		}
+
+		// 핵심: Viewport 최상단에 올림
+		FloatingWidget->AddToViewport(9999);
+	}
+
+	FloatingWidget->SetRecoveryText(Amount, bIsMana);
+	FloatingWidget->SetRecoveryAlpha(1.0f);
+
+	UpdateWidgetScreenPosition();
+}
+
+void AW_FloatingRecoveryTextActor::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	ElapsedTime += DeltaSeconds;
+
+	// 액터 위치를 위로 올려서 텍스트가 떠오르는 느낌 유지
+	FVector NewLocation = GetActorLocation();
+	NewLocation.Z += FloatUpSpeed * DeltaSeconds;
+	SetActorLocation(NewLocation);
+
+	// 화면 좌표 갱신
+	UpdateWidgetScreenPosition();
+
+	// 알파 감소
+	const float Alpha = 1.0f - (ElapsedTime / FMath::Max(LifeSeconds, KINDA_SMALL_NUMBER));
+	if (FloatingWidget)
+	{
+		FloatingWidget->SetRecoveryAlpha(Alpha);
+	}
+
+	// 수명 종료
+	if (ElapsedTime >= LifeSeconds)
+	{
+		Destroy();
+	}
+}
+
+void AW_FloatingRecoveryTextActor::UpdateWidgetScreenPosition()
+{
+	if (!FloatingWidget || !CachedLocalPC)
+	{
+		return;
+	}
+
+	FVector2D ScreenPosition;
+	const bool bProjected = UGameplayStatics::ProjectWorldToScreen(
+		CachedLocalPC,
+		GetActorLocation(),
+		ScreenPosition,
+		true
+	);
+
+	if (!bProjected)
+	{
+		return;
+	}
+
+	ScreenPosition.Y += ScreenOffsetY;
+
+	FloatingWidget->SetPositionInViewport(ScreenPosition, true);
+}
+
+void AW_FloatingRecoveryTextActor::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (FloatingWidget)
+	{
+		FloatingWidget->RemoveFromParent();
+		FloatingWidget = nullptr;
+	}
+
+	Super::EndPlay(EndPlayReason);
 }
